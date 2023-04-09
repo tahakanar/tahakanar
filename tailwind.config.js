0168f824e2de3cf483f8f6fc156e0f8af89c2607
tahakanar <tahakanar@gmail.com>
2023-04-10 00:42:40 +0300

    porfolio page edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const theme = require("tailwindcss/defaultTheme");
+const { colors } = require("./tailwind.config.shared");
+
 module.exports = {
+  mode: "jit",
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors,
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['"Open Sans"', ...theme.fontFamily.sans],
+        montserrat: ['"Montserrat"', ...theme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
-}
+};
