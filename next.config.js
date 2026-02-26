1c208c23481743a54f54f1b17f3e6fda6acc13a0
Taha Kanar <>
2026-02-26 13:40:59 +0300

    master:add deploying process as blog page

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// output: "export",
-	experimental: {
-		viewTransition: true
-	}
+  // output: "export",
+  experimental: {
+    viewTransition: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.tahakanar.com' }],
+        destination: 'https://tahakanar.com/:path*',
+        permanent: true
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
