d904c6c056a9fabb44b372004c0dc912e9ba6b94
tahakanar <taha.kanar@akinon.com>
2026-03-05 21:26:49 +0300

    master:update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
 	// output: "export",
 	experimental: {
 		viewTransition: true
+	},
+	async redirects() {
+		return [
+			{
+				source: '/:path*',
+				has: [{ type: 'host', value: 'www.tahakanar.com' }],
+				destination: 'https://tahakanar.com/:path*',
+				permanent: true
+			}
+		];
 	}
 };
 
